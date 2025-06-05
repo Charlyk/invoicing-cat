d86404a1b1f94e4e8bd08a153d0db4ad907d62db
Eduard Albu <eduard.albu@gmail.com>
2025-06-05 16:22:06 +0300

    Fixed layout for mobile version

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,44 +1,44 @@
-import { Container, HStack, Icon, Link, Stack } from '@chakra-ui/react'
+import {Container, HStack, Icon, Link, Stack} from '@chakra-ui/react'
 import {SiGithub, SiInstagram, SiLinkedin, SiReddit, SiX} from 'react-icons/si'
-import { Copyright } from './copyright'
+import {Copyright} from './copyright'
 import {Logo} from "@/components/nav-bar/logo";
 
 export const Footer = () => (
-  <Container as="footer" py={{ base: '10', md: '12' }}>
-    <Stack gap="6">
-      <Stack direction="row" justify="space-between" align="center">
-        <Logo />
-        <HStack gap="4">
-          {navLinks.map(({ href, label }, index) => (
-              <Link key={index} href={href} colorPalette="gray">
-                {label}
-              </Link>
-          ))}
-        </HStack>
-        <HStack gap="4">
-          {socialLinks.map(({ href, icon }, index) => (
-            <Link key={index} href={href} colorPalette="gray">
-              <Icon size="md">{icon}</Icon>
-            </Link>
-          ))}
-        </HStack>
-      </Stack>
-      <Copyright />
-    </Stack>
-  </Container>
+    <Container as="footer" py={{base: '10', md: '12'}}>
+        <Stack gap="6">
+            <Stack direction="row" justify="space-between" align="center">
+                <Logo/>
+                <HStack gap="4">
+                    {socialLinks.map(({href, icon}, index) => (
+                        <Link key={index} href={href} colorPalette="gray">
+                            <Icon size="md">{icon}</Icon>
+                        </Link>
+                    ))}
+                </HStack>
+            </Stack>
+            <HStack gap="4" justify="center">
+                {navLinks.map(({href, label}, index) => (
+                    <Link key={index} href={href} colorPalette="gray">
+                        {label}
+                    </Link>
+                ))}
+            </HStack>
+            <Copyright/>
+        </Stack>
+    </Container>
 )
 
 const socialLinks = [
-  { href: 'https://x.com/eduardalbu', icon: <SiX /> },
-  { href: 'https://github.com/Charlyk', icon: <SiGithub /> },
-  { href: 'https://www.linkedin.com/in/eduard-albu-4a156698/', icon: <SiLinkedin /> },
-  { href: 'https://www.instagram.com/eduard.albu.dev', icon: <SiInstagram /> },
-  { href: 'https://www.reddit.com/user/eduardalbu/', icon: <SiReddit /> },
+    {href: 'https://x.com/eduardalbu', icon: <SiX/>},
+    {href: 'https://github.com/Charlyk', icon: <SiGithub/>},
+    {href: 'https://www.linkedin.com/in/eduard-albu-4a156698/', icon: <SiLinkedin/>},
+    {href: 'https://www.instagram.com/eduard.albu.dev', icon: <SiInstagram/>},
+    {href: 'https://www.reddit.com/user/eduardalbu/', icon: <SiReddit/>},
 ]
 
 const navLinks = [
-  { href: '/en', label: 'English' },
-  { href: '/fr', label: 'Français' },
-  { href: '/de', label: 'Deutsch' },
-  { href: '/pt', label: 'Português' },
+    {href: '/en', label: 'English'},
+    {href: '/fr', label: 'Français'},
+    {href: '/de', label: 'Deutsch'},
+    {href: '/pt', label: 'Português'},
 ];
