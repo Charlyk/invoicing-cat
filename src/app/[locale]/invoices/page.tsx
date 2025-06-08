d27b8e53153be1dc909f55f51ccc79cc8ea809c7
Eduard Albu <eduardalbu@gmail.com>
2025-06-09 00:02:29 +0300

    Updated metadata

--- src/app/[locale]/invoices/page.tsx
+++ src/app/[locale]/invoices/page.tsx
@@ -13,7 +13,7 @@ export function generateStaticParams() {
 
 export async function generateMetadata({ params }: { params: Promise<{ locale: string }> }): Promise<Metadata> {
   const { locale } = await params;
-  const t = await getTranslations({ locale, namespace: 'ClientsList' });
+  const t = await getTranslations({ locale, namespace: 'InvoicesPage' });
 
   return {
     metadataBase: new URL('https://invoicingcat.com'),
